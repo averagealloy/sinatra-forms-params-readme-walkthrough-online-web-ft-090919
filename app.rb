9d1bec10fc120d8a532f1fa07d4237359348b00a
Mike McTighe <m.mctighe10@gmail.com>
2019-10-23 17:57:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post '/food' do
-    
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-    erb :food_form
   end
+
 end
